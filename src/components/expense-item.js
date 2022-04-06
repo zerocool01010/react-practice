127229b4a9b928e57619c2514be4fa1b24399a04
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-04-06 17:43:06 -0300

    Agregado un simple onClick handler para hacer un console.log en expense-item

--- src/components/expense-item.js
+++ src/components/expense-item.js
@@ -25,12 +25,18 @@ const ExpenseItem = (props) => {
 
     const props2 = props; //para el componente hijo en expDate.js
 
+    const clickHandlerEvent = () => {
+        console.log('Clicked');
+    }
+
     return <Card className='expense-item'> {/* al agregar el componente Card con estilos definidos, el componente ya no soporta el className, solo lo que trae Card, y si queremos que soporte el className, tenemos que pasarlo a traves de props */}
             <ExpenseDate dateChild={props2.date}/>   
         <div className='expense-item__description'> {/* pero el resto de custom comps default (jsx o falso html) sigue soportando el estilo de los className */}
             <h2>{expenseTitle}</h2>
         </div>
         <div className='expense-item__price'>${expenseAmount}</div>
+        <button onClick={clickHandlerEvent}/* aca no estamos llamando a la funcion porque no hay parentesis */>
+            Change the title</button> {/* en su lugar estamos generando un apuntador que apunta a la funcion mas arriba declarada */}
     </Card>;
 }
 
