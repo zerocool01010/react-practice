fe856ae27008e3df55d58bf2d6831cb6e41b510e
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-04-08 14:15:50 -0300

    Agregado un filtrador por anio sin mucha funcionalidad por ahora con su css. Renderizo el array expenses para pasarselo al componente ExpenseItem usando el .map() y lo hago generando ids dinamicos con otra funcion. Esto ultimo no es del curso, es un agregado personal

--- src/components/expenses.js
+++ src/components/expenses.js
@@ -1,39 +1,101 @@
 import ExpenseItem from './expense-item';
 import './expenses.css';
 import Card from './cards/Card';
+import { useState } from 'react';
+import ExpensesFilter from './expensesFilter';
 
-const expenses = () => {
-    const expenses = [
-        {
-          id: 'e1',
-          title: 'Toilet Paper',
-          amount: Math.random() * 10,
-          date: new Date(2020, 7, 14),
-        },
-        { id: 'e2', 
+function expensesArray(){
+
+  let finalExpenses = [];
+
+  function generateExpWithDynamicIds(){
+    let expenses = [
+      {
+        id: 'e',
+        title: 'Toilet Paper',
+        amount: Math.random() * 10,
+        date: new Date(2020, 7, 14),
+      },
+      { 
+        id: 'e', 
         title: 'New TV', 
         amount: Math.random() * 10, 
-        date: new Date(2021, 2, 12)},
-        {
-          id: 'e3',
-          title: 'Car Insurance',
-          amount: Math.random() * 10,
-          date: new Date(2021, 2, 28),
-        },
-      ];
+        date: new Date(2021, 2, 12)
+      },
+      {
+        id: 'e',
+        title: 'Car Insurance',
+        amount: Math.random() * 10,
+        date: new Date(2021, 3, 28),
+      },
+      {
+        id: 'e',
+        title: 'PlayStation 5',
+        amount: Math.random() * 10,
+        date: new Date(2019, 6, 22),
+      },
+      {
+        id: 'e',
+        title: 'Acer computer',
+        amount: Math.random() * 10,
+        date: new Date(2022, 11, 19),
+      },
+      {
+        id: 'e',
+        title: 'Dell computer',
+        amount: Math.random() * 10,
+        date: new Date(2022, 12, 21),
+      },
+      {
+        id: 'e',
+        title: 'Mac computer',
+        amount: Math.random() * 10,
+        date: new Date(2019, 8, 8),
+      },
+      {
+        id: 'e',
+        title: 'Bangho computer',
+        amount: Math.random() * 10,
+        date: new Date(2020, 5, 17),
+      },
+    ];
+
+    let idD = 1;
+    for (const expense of expenses) {
+      if (expense.id){
+        expense.id = 'e'+(idD.toString());
+        idD++
+      }
+    }
+    return expenses;
+  }
+
+  finalExpenses = generateExpWithDynamicIds();
+  console.log(finalExpenses);
+
+  return finalExpenses;
+}
+  
+
+const Expenses = () => {
+  const expenses = expensesArray();
+
+  const [yearFiltered, setFilteredYear] = useState('2000')
+
+  const filterChangeHandler = yearSelected => {
+    setFilteredYear(yearSelected);
+    console.log(yearSelected);
+  }
+  const expensesMapped = expenses.map((expense, index) => 
+  <ExpenseItem name={expense.title}
+  price={expense.amount}
+  date={expense.date}/>)
     
-      return <Card className="expenses">
-      <ExpenseItem name={expenses[0].title} 
-      price={expenses[0].amount} 
-      date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem name={expenses[1].title} 
-      price={expenses[1].amount} 
-      date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem name={expenses[2].title} 
-      price={expenses[2].amount} 
-      date={expenses[2].date}></ExpenseItem>
-      </Card>
-      ;
+  return <Card className="expenses">
+    <ExpensesFilter selected={yearFiltered} onChangeFilter={filterChangeHandler}/> 
+    <>{expensesMapped}</>
+  </Card>
+  ;
 }
 
-export default expenses;
+export default Expenses;
---
+++ src/components/expensesFilter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './expensesFilter.css';
+
+const ExpensesFilter = props => {
+    const dropDownChangeHandler = (event) => {
+        props.onChangeFilter(event.target.value);
+    }
+
+    return (
+        <div className='expenses__filter'>
+            <div className='expenses_filt'>
+                <label>Filter by year</label>
+                <select value={props.selected} onChange={dropDownChangeHandler}>
+                    <option value='2022'>2022</option>
+                    <option value='2021'>2021</option>
+                    <option value='2020'>2020</option>
+                    <option value='2019'>2019</option>
+                </select>
+            </div>
+        </div>
+    )
+}
+
+export default ExpensesFilter
